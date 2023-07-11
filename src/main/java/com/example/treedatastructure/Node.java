d427fcfa18418b5a4967bcc517a3756462c96585
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2023-07-11 15:55:22 +0700

    add button specification

---
+++ src/main/java/com/example/treedatastructure/Node.java
@@ -0,0 +1,4 @@
+package com.example.treedatastructure;
+
+public class Node {
+}
--- src/main/java/com/example/treedatastructure/genericTreeController.java
+++ src/main/java/com/example/treedatastructure/genericTreeController.java
@@ -6,20 +6,33 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class genericTreeController {
 
     @FXML
-    private Button btnDelete;
+    private Button btnOpsCreate;
 
     @FXML
-    private Button btnInsert;
+    private Button btnOpsInsert;
+
+    @FXML
+    private Button btnOpsDelete;
+
+    @FXML
+    private Button btnOpsUpdate;
+
+    @FXML
+    private Button btnOpsTraverse;
 
     @FXML
     private StackPane stackPaneInput;
@@ -39,6 +52,68 @@ public class genericTreeController {
     @FXML
     private HBox hBoxTraverse;
 
+    @FXML
+    private RadioButton radioBtnBFS;
+
+    @FXML
+    private RadioButton radioBtnDFS;
+
+    @FXML
+    private TextField tfNodeTraverse;
+
+    @FXML
+    private Button btnTraverse;
+
+    @FXML
+    private HBox hBoxDelete;
+
+    @FXML
+    private TextField tfNodeDelete;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private HBox hBoxInsert;
+
+    @FXML
+    private HBox hBoxSearch;
+
+    @FXML
+    private TextField tfParentInsert;
+
+    @FXML
+    private TextField tfNodeInsert;
+
+    @FXML
+    private Button btnInsert;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private HBox hBoxUpdate;
+
+    @FXML
+    private TextField tfOldNodeUpdate;
+
+    @FXML
+    private TextField tfNewNodeUpdate;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private VBox vBoxPseudo;
+
+    @FXML
+    private StackPane stackPanePseudo;
+
+    @FXML
+    private StackPane stackPaneController;
+
+
+
 
     private Stage menuStage;
 
@@ -49,61 +124,119 @@ public class genericTreeController {
     @FXML
     private void initialize() {
         stackPaneInput.setVisible(false);
+        stackPanePseudo.setVisible(false);
+        stackPaneController.setVisible(false);
     }
 
 
     @FXML
     void btnOpsInsertPressed(ActionEvent event) {
         stackPaneInput.setVisible(true);
+
+        setControl(hBoxInsert);
+
     }
 
     @FXML
     void btnOpsDeletePressed(ActionEvent event) {
         stackPaneInput.setVisible(true);
+
+        setControl(hBoxDelete);
     }
 
     @FXML
     void btnOpsCreatePressed(ActionEvent event) {
         stackPaneInput.setVisible(true);
+
+        setControl(hBoxCreate);
     }
 
     @FXML
     void btnOpsUpdatePressed(ActionEvent event) {
         stackPaneInput.setVisible(true);
+
+        setControl(hBoxUpdate);
     }
 
     @FXML
     void btnOpsTraversePressed(ActionEvent event) {
         stackPaneInput.setVisible(true);
+
+        setControl(hBoxTraverse);
     }
 
     @FXML
-    void radioBtnRandomPressed(ActionEvent event) {
+    void btnOpsSearchPressed(ActionEvent event) {
+        stackPaneInput.setVisible(true);
 
+        setControl(hBoxSearch);
     }
 
     @FXML
-    void radioBtnManualPressed(ActionEvent event) {
-
-    }
+    private void radioBtnRandomPressed(ActionEvent event) {}
 
     @FXML
-    void tfRootCreateTyping(ActionEvent event) {
-
-    }
+    private void radioBtnManualPressed(ActionEvent event) {}
 
     @FXML
-    void btnCreatePressed(ActionEvent event) {
-
-    }
+    private void tfRootCreateTyping(ActionEvent event) {}
 
     @FXML
-    void radioBtnBFSPressed(ActionEvent event) {
+    private void btnCreatePressed(ActionEvent event) {}
 
+    @FXML
+    private void radioBtnBFSPressed(ActionEvent event) {}
+
+    @FXML
+    private void radioBtnDFSPressed(ActionEvent event) {}
+
+    @FXML
+    private void tfNodeTraverseTyping(ActionEvent event) {}
+
+    @FXML
+    private void btnTraversePressed(ActionEvent event) {}
+
+    @FXML
+    private void tfNodeDeleteTyping(ActionEvent event) {}
+
+    @FXML
+    private void btnDeletePressed(ActionEvent event) {}
+
+    @FXML
+    private void tfParentInsertTyping(ActionEvent event) {}
+
+    @FXML
+    private void tfNodeInsertTyping(ActionEvent event) {}
+
+    @FXML
+    private void btnInsertPressed(ActionEvent event) {}
+
+    @FXML
+    private void tfOldNodeUpdateTyping(ActionEvent event) {}
+
+    @FXML
+    private void tfNewNodeUpdateTyping(ActionEvent event) {}
+
+    @FXML
+    private void btnUpdatePressed(ActionEvent event) {}
+
+    @FXML
+    void btnSearchPressed(ActionEvent event) {}
+
+    @FXML
+    void tfNodeSearchTyping(ActionEvent event) {}
+
+    private void setControl(HBox hBoxOn) {
+        ArrayList<HBox> setHBox = new ArrayList<HBox>(Arrays.asList(hBoxCreate, hBoxTraverse, hBoxInsert, hBoxDelete, hBoxUpdate, hBoxSearch));
+        for (HBox hbox: setHBox) {
+            if (hBoxOn.equals(hbox)) {
+                hbox.setVisible(true);
+            }
+            else {
+                hbox.setVisible(false);
+            }
+        }
     }
-
-
-
 
 
 
